654c4614b52c4dae611bf5af62ef58d3eb82c2c3
glucion05012 <giddel.lucion@yahoo.com>
2024-10-30 13:38:29 +0800

    first commit

---
+++ application/views/templates/navbar.php
@@ -0,0 +1,82 @@
+<div class="wrapper">
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="<?php echo base_url(); ?>home" class="nav-link">Home</a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav ml-auto">
+    
+      <li class="dropdown user user-menu">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+              <i class="fas fa-user-tie nav-icon"></i>
+             FULL NAME
+            </a>
+            <ul class="dropdown-menu">
+              <!-- Menu Footer-->
+              <li class="user-footer">
+                <div class="pull-right">
+                  <a href="<?php echo base_url(); ?>logincontroller/logout" class="btn btn-default btn-flat">Sign out</a>
+                </div>
+              </li>
+            </ul>
+          </li>
+          <!-- Control Sidebar Toggle Button -->
+          <!-- <li>
+            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
+          </li> -->
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="<?php echo base_url(); ?>home" class="brand-link">
+      <img src="<?php echo base_url()."assets/"; ?>denr-logo.png" alt="Logo" class="brand-image "
+           style="opacity: .8">
+      <span class="brand-text font-weight-light" style="font-size:14px">Client Registration System</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+     
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">  
+
+            <li class="nav-header">Permits</li>
+            
+            <li class="nav-item has-treeview">
+                <a href="#" class="nav-link">
+                  <i class="nav-icon fa fa-tree"></i>
+                  <p>
+                    Forest Utilization
+                    <i class="fas fa-angle-right right"></i>
+                  </p>
+                </a>
+                
+                <ul class="nav nav-treeview">
+
+                  <li class="nav-item">
+                      <a href="<?php echo $_SESSION['base_url_tcp']; ?>" class="nav-link">
+                      <i class="nav-icon fa fa-scissors"></i>
+                        <p>Tree Cutting Permit</p>
+                      </a>
+                  </li>
+
+                </ul>
+            </li>
+            
+        </ul>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
